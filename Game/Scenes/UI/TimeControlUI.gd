d2d7640f3b55149e84d06ed12af10b59b7913d52
craig <craigostrin@gmail.com>
2021-09-11 14:34:39 -0700

    Add UI for time control (placeholder assets)
    Uses a tween to animate the ClockHand when a new time is set
    Time controls should probably live on the Player (to keep input in one place, and also because you can't control time when the Player doesn't exist)
    
    (PS: Not sure why my GitHub is detecting a bunch of whitespace changes for objects I didn't touch!)

---
+++ Game/Scenes/UI/TimeControlUI.gd
@@ -0,0 +1,43 @@
+extends Control
+
+# We may need a signal for when the ClockHand finishes animation
+
+var current_time: int = 12 setget set_current_time
+export var hand_rotate_time := 1.0
+
+onready var clock_hand: TextureRect = $ClockHand
+onready var t: Tween = $Tween
+
+
+# For DEBUG --> later will operate on signals
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("progress_time"):
+		self.current_time += 1
+	if event.is_action_pressed("regress_time"):
+		self.current_time -= 1
+
+
+func set_current_time(val : int) -> void:
+	var old_time := current_time
+	val = wrapi(val, 1, 13)
+	current_time = val
+	_set_clock_hand(current_time)
+
+
+func _set_clock_hand(new_time: int) -> void:
+	var degrees := 30
+	# Use current rotation so there's no skipping if the player changes the time while
+	# the hand is already moving
+	var start := clock_hand.rect_rotation
+	var target := new_time * degrees
+	var rotation := _fix_rotation(target - start)
+	t.interpolate_property(clock_hand, "rect_rotation", start, start + rotation, hand_rotate_time)
+	t.start()
+
+
+# Used to make sure the ClockHand always uses the shortest rotation
+# For example, instead of moving +330 degrees to go from 1 to 12, it will only move -30 degrees
+func _fix_rotation(rotation: float) -> float:
+	if abs(rotation) > 180:
+		rotation = wrapi(rotation - 360, -180, 180)
+	return rotation
